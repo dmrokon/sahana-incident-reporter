ccb1f993b1c3bc63e1f0c33eddf7d1f4b59f4cc8
mnagda <mnagda@89940443-54d2-3b09-a486-d625dbe5e16f>
2010-10-26 23:03:16 +0000

    moved to mnagda

---
+++ SahanaIncidentReporter/src/cmu/mobilelab/Reporter.java
@@ -0,0 +1,26 @@
+package cmu.mobilelab;
+
+public class Reporter {
+	
+	private String	reporterName; 
+	private String 	contactDetails;
+	
+	public Reporter(String ReporterName, String ContactDetails)
+	{
+		reporterName = ReporterName; 
+		contactDetails = ContactDetails; 
+	}
+	
+	public void setReporterName(String reporterName) {
+		this.reporterName = reporterName;
+	}
+	public String getReporterName() {
+		return reporterName;
+	}
+	public void setContactDetails(String contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+	public String getContactDetails() {
+		return contactDetails;
+	} 
+}

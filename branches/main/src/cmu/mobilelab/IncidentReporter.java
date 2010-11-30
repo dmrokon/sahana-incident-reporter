b3abcf0d22fc18b7c951ba41a1da1ae2b8807279
mshuster@gmail.com <mshuster@gmail.com@89940443-54d2-3b09-a486-d625dbe5e16f>
2010-11-30 03:20:23 +0000

    Shared Prefs, Global menu options, impact long-click remove.

--- branches/main/src/cmu/mobilelab/IncidentReporter.java
+++ branches/main/src/cmu/mobilelab/IncidentReporter.java
@@ -1,11 +1,24 @@
 package cmu.mobilelab;
 
+import android.app.AlertDialog;
 import android.app.TabActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.TableRow.LayoutParams;
 
 
 public class IncidentReporter extends TabActivity {
@@ -14,6 +27,7 @@ public class IncidentReporter extends TabActivity {
 	private final static String SUMMARY_TAG = "summary"; 
 	private final static String LIST_TAG = "reports"; 
 	private final static String MAP_TAG = "maps";
+	public static final String SHARED_PREFERENCES = "incidentreporter_prefs";
 	
     /** Called when the activity is first created. */
     @Override
@@ -111,4 +125,96 @@ public class IncidentReporter extends TabActivity {
     	else
     		return ""; //this should throw an exception actually
     }    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.form_menu, menu);
+        return true;
+    }
+    
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		return true;	
+	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.menu_about:
+            //TODO:About activity
+        	AlertDialog.Builder about_builder = new AlertDialog.Builder(this);
+    		about_builder.setCancelable(true).setTitle("About").setMessage(R.string.about);
+    		AlertDialog about_alert = about_builder.create();
+    		about_alert.show();
+            return true;
+        case R.id.menu_preferences:
+            //TODO:preferences activity
+    	    
+            SharedPreferences settings = getSharedPreferences(SHARED_PREFERENCES, 0);
+            String reporter_name = settings.getString("reporter_name", null);
+            String reporter_contact = settings.getString("reporter_contact", null);
+            Log.i("reporter_name", reporter_name);
+            
+        	
+    		final EditText reporter_name_edittext = new EditText(this);
+    		final EditText reporter_contact_edittext = new EditText(this);
+            
+            if (reporter_name.length() > 1) {
+            	reporter_name_edittext.setText(reporter_name);
+            	reporter_contact_edittext.setText(reporter_contact);
+            }
+
+    		//create textview labels
+    		TextView reporter_name_label = new TextView(this);
+    		reporter_name_label.setText("Name");
+    		
+    		TextView reporter_contact_label = new TextView(this);
+    		reporter_contact_label.setText("Email Address");
+    		
+    		//programatically implement layout widget
+    		LinearLayout reporter_widget = new LinearLayout(this);
+    		reporter_widget.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT ));
+    		reporter_widget.setOrientation(LinearLayout.VERTICAL);
+    		reporter_widget.setPadding(5,5,5,5);
+    		reporter_widget.addView(reporter_name_label);
+    		reporter_widget.addView(reporter_name_edittext);
+    		reporter_widget.addView(reporter_contact_label);
+    		reporter_widget.addView(reporter_contact_edittext);
+    		
+    		AlertDialog.Builder reporter_builder = new AlertDialog.Builder(this);
+    		reporter_builder.setMessage("Please save your contact information")
+    		       .setCancelable(true)
+    		       .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+    		           public void onClick(DialogInterface dialog, int id) {
+    		        	   String reporter_name = reporter_name_edittext.getText().toString();
+    		        	   String reporter_contact = reporter_contact_edittext.getText().toString();
+    		        	   
+    		        	   SharedPreferences settings = getSharedPreferences(SHARED_PREFERENCES, 0);
+    		        	   SharedPreferences.Editor editor = settings.edit();
+    		        	   editor.putString("reporter_name", reporter_name);
+    		        	   editor.putString("reporter_contact", reporter_contact);
+    		        	   editor.commit();
+    		        	   
+    		        	   //TODO:Set Shared Preferences    		        	   
+    		          }
+    		       })
+    		       .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+    		           public void onClick(DialogInterface dialog, int id) {
+    		               //return nothing if the dialog is canceled 
+    		        	   dialog.cancel();
+    		           }
+    		       })
+    		       .setView(reporter_widget); //add the display widget to the dialog
+    		
+    		AlertDialog reporter_alert = reporter_builder.create();
+    		//launch the alert dialog
+    		reporter_alert.show();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
